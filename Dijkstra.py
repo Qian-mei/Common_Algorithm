06228b309b1becddf08da454d88202f1bfdd55c7
Qian-mei <1002180276@qq.com>
2021-09-02 02:49:48 +0800

    Dijistra

--- Dijkstra.py
+++ Dijkstra.py
@@ -2,12 +2,12 @@ import sys
 # 虽然都适用于无向和有向连通图，但仅仅适用于单源点开始最短路径
 def prim(g,n):
     dis = [0]*n
-    pre = [-2]*n
+    pre = [-2]*n #可删除
     flag = [False]*n
     flag[0] = True
 # 假设从index 0 即 A 出发搜寻
     k = 0
-    pre[k] = -1
+    pre[k] = -1 #可删除
 # dis[i] 记录到i到A的最小距离，迭代更新 // 与Prim的差异
     for i in range(n):
         dis[i] = g[k][i]
@@ -28,8 +28,8 @@ def prim(g,n):
             if dis[i]>dis[k]+g[k][i] and not flag[i]:
 # 更新dis[i]
                 dis[i] = dis[k] + g[k][i]
-                pre[i] = k
-    return dis,pre
+                pre[i] = k #可删除
+    return dis,pre #可删除pre
 
 
 if __name__ == "__main__":
@@ -47,10 +47,10 @@ if __name__ == "__main__":
              [_,_,_,_,0,_],
              [_,2,_,6,1,0]]
     n = len(graph)
-    dis,pre = prim(graph,n)
+    dis,pre = prim(graph,n) #可删除pre
     dic = {-2:'Second',-1:'First'}
     for i in range(n):
         dic[i] = chr(i+65)
     for i in range(n):
-        print('A-->',dic[i],'PRE:<--',dic[pre[i]],': ',dis[i])
+        print('A-->',dic[i],'PRE:<--',dic[pre[i]],': ',dis[i]) #可删除pre
     
